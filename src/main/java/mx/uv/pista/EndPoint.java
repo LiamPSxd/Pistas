25e381eabe0d45d48ccc2219b06508ca5e1e5977
LiamPSxd <zS20018158@estudiantes.uv.mx>
2023-05-13 20:01:26 -0600

    Correcciones

--- src/main/java/mx/uv/pista/EndPoint.java
+++ src/main/java/mx/uv/pista/EndPoint.java
@@ -1,5 +1,8 @@
 package mx.uv.pista;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
@@ -21,76 +24,79 @@ public class EndPoint{
     private IPista iPista;
     private JSONObject json;
 
-    @PayloadRoot(localPart = "GETPistaRequest", namespace = "https://t4is.uv.mx/pistas")
-    @ResponsePayload
-    public GETPistaResponse GET(@RequestPayload GETPistaRequest request){
-        GETPistaResponse response = new GETPistaResponse();
-        String res = "{\"message\": \"Exitoso\", \"data\": [";
+    // @PayloadRoot(localPart = "GETPistaRequest", namespace = "https://t4is.uv.mx/pistas")
+    // @ResponsePayload
+    // public GETPistaResponse GET(@RequestPayload GETPistaRequest request){
+    //     GETPistaResponse response = new GETPistaResponse();
+    //     List<Pista> pistas = new ArrayList<Pista>();
+    //     // String res = "{\"message\": \"Exitoso\", \"data\": [";
 
-        if(request.getId() == -1)
-            for(Pista pista : iPista.findAll()) res += pista.toString();
-        else if(request.getId() > -1){
-            Pista pista = iPista.findById(request.getId()).get();
+    //     if(request.getId() == -1)
+    //         for(Pista pista : iPista.findAll()) pistas.add(pista);
+    //         response.setStatus("Exitoso");
+    //         response.setPista(null);
+    //     else if(request.getId() > -1){
+    //         Pista pista = iPista.findById(request.getId()).get();
 
-            if(pista != null) res += pista.toString();
-            else res = "{\"message\": \"Fallido\", \"data\": [";
-        }
+    //         if(pista != null) pistas.add(pista);
+    //         else res = "{\"message\": \"Fallido\", \"data\": [";
+    //     }
 
-        json = new JSONObject(res + "]}");
-        response.setReturn(json.toString());
+    //     json = new JSONObject(res + "]}");
+    //     response.setReturn(json.toString());
 
-        return response;
-    }
+    //     return response;
+    // }
 
-    @PayloadRoot(localPart = "POSTPistaRequest", namespace = "https://t4is.uv.mx/pistas")
-    @ResponsePayload
-    public POSTPistaResponse POST(@RequestPayload POSTPistaRequest request){
-        POSTPistaResponse response = new POSTPistaResponse();
+    // @PayloadRoot(localPart = "POSTPistaRequest", namespace = "https://t4is.uv.mx/pistas")
+    // @ResponsePayload
+    // public POSTPistaResponse POST(@RequestPayload POSTPistaRequest request){
+    //     POSTPistaResponse response = new POSTPistaResponse();
 
-        Pista pista = new Pista();
-        pista.setEstado(request.isEstado());
-        iPista.save(pista);
+    //     Pista pista = new Pista();
+    //     pista.setEstado(request.isEstado());
+    //     iPista.save(pista);
 
-        if(iPista.findById(pista.getId()).get() != null) json = new JSONObject("{\"message\": \"Exitoso\"}");
-        else json = new JSONObject("{\"message\": \"Fallido\"}");
+    //     if(iPista.findById(pista.getId()).get() != null) json = new JSONObject("{\"message\": \"Exitoso\"}");
+    //     else json = new JSONObject("{\"message\": \"Fallido\"}");
 
-        response.setReturn(json.toString());
+    //     response.setReturn(json.toString());
 
-        return response;
-    }
+    //     return response;
+    // }
 
-    @PayloadRoot(localPart = "PUTPistaRequest", namespace = "https://t4is.uv.mx/pistas")
-    @ResponsePayload
-    public PUTPistaResponse PUT(@RequestPayload PUTPistaRequest request){
-        PUTPistaResponse response = new PUTPistaResponse();
-        Pista pista = iPista.findById(request.getId()).get();
+    // @PayloadRoot(localPart = "PUTPistaRequest", namespace = "https://t4is.uv.mx/pistas")
+    // @ResponsePayload
+    // public PUTPistaResponse PUT(@RequestPayload PUTPistaRequest request){
+    //     PUTPistaResponse response = new PUTPistaResponse();
+    //     Pista pista = iPista.findById(request.getId()).get();
 
-        if(pista != null){
-            pista.setEstado(request.isEstado());
-            iPista.save(pista);
+    //     if(pista != null){
+    //         pista.setEstado(request.isEstado());
+    //         iPista.save(pista);
 
-            json = new JSONObject("{\"message\": \"Exitoso\"}");
-        }else json = new JSONObject("{\"message\": \"Fallido\"}");
+    //         json = new JSONObject("{\"message\": \"Exitoso\"}");
+    //     }else json = new JSONObject("{\"message\": \"Fallido\"}");
 
-        response.setReturn(json.toString());
+    //     response.setReturn(json.toString());
 
-        return response;
-    }
+    //     return response;
+    // }
 
-    @PayloadRoot(localPart = "DELETEPistaRequest", namespace = "https://t4is.uv.mx/pistas")
-    @ResponsePayload
-    public DELETEPistaResponse DELETE(@RequestPayload DELETEPistaRequest request){
-        DELETEPistaResponse response = new DELETEPistaResponse();
-        Pista pista = iPista.findById(request.getId()).get();
+    // @PayloadRoot(localPart = "DELETEPistaRequest", namespace = "https://t4is.uv.mx/pistas")
+    // @ResponsePayload
+    // public DELETEPistaResponse DELETE(@RequestPayload DELETEPistaRequest request){
+    //     DELETEPistaResponse response = new DELETEPistaResponse();
+    //     Pista pista = iPista.findById(request.getId()).get();
 
-        if(pista != null){
-            iPista.delete(pista);
+    //     if(pista != null){
+    //         iPista.delete(pista);
 
-            json = new JSONObject("{\"message\": \"Exitoso\"}");
-        }else json = new JSONObject("{\"message\": \"Fallido\"}");
+    //         json = new JSONObject("{\"message\": \"Exitoso\"}");
+    //     }else json = new JSONObject("{\"message\": \"Fallido\"}");
 
-        response.setReturn(json.toString());
+    //     response.setReturn(json.toString());
 
-        return response;
-    }
+    //     return response;
+    // }
 }
